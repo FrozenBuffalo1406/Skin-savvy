f6a43e8195297a798829009f1060d2ffcaeea411
FrozenBuffalo1406 <ahmadfauzirizky003@gmail.com>
2024-06-03 07:19:17 +0700

    add image slider adapter

---
+++ app/src/main/java/com/capstone/skinsavvy/data/ImageSliderItem.kt
@@ -0,0 +1,3 @@
+package com.capstone.skinsavvy.data
+
+data class ImageSliderItem(val imageUrl : String)
---
+++ app/src/main/java/com/capstone/skinsavvy/viewmodel/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.capstone.skinsavvy.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+//ngambil dari api variasi obat-obatan
+class MainViewModel : ViewModel(){
+    private lateinit var viewModel: MainViewModel
+
+}
