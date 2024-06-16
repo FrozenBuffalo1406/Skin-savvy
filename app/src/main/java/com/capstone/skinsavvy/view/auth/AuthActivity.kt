e0fc7ce68be7404e58cf77b9e6a754302838d53b
FrozenBuffalo1406 <ahmadfauzirizky003@gmail.com>
2024-06-16 21:27:29 +0700

    repair repo

--- app/src/main/java/com/capstone/skinsavvy/view/auth/AuthActivity.kt
+++ app/src/main/java/com/capstone/skinsavvy/view/auth/AuthActivity.kt
@@ -1,16 +1,17 @@
-package com.capstone.skinsavvy
+package com.capstone.skinsavvy.view.auth
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.capstone.skinsavvy.R
 
-class ProfileActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_auth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/capstone/skinsavvy/view/signin/SignInActivity.kt
+++ app/src/main/java/com/capstone/skinsavvy/view/signin/SignInActivity.kt
@@ -1,10 +1,8 @@
-package com.capstone.skinsavvy.view
+package com.capstone.skinsavvy.view.signin
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.capstone.skinsavvy.R
 
 class SignInActivity : AppCompatActivity() {
